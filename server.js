dcb93f04caea786519815a7d393242fa8f87443a
Fstp <fstp@Fstpde-MacBook-Pro.local>
2019-05-25 00:57:05 +0800

    003

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var fs = require('fs');
 var file = fs.createWriteStream('test2.mp3', { encoding: 'binary' });
 var net = require('net');
+var http=require('http');
 var HOST = '127.0.0.1';
 var PORT = 8877;
-
 var server = net.createServer();
+
+http.createServer((req,res)=>{
+
+        //mp.pipe(res);
+        //res.writeHead(200,{
+        //      'Content-Type':'audio/mpeg',
+        //      })
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("Hello World");
+        res.end();
+}).listen(3000)
+console.log('Server is running at 127.0.0.1:3000')
 
 server.on('listening', function(){
   console.log('Server is listening on port', PORT);
@@ -20,11 +32,12 @@ server.on('connection', function(socket){
   console.log('client IP：' + socket.remoteAddress);
   console.log('client PORT：' + socket.remotePort);
   //data event
-  socket.on('data', function(data){
+  global.mp = socket.on('data', function(data){
     //data.pipe(file);
     console.info('data is %b', data);
     console.info('socket.bytesRead is ' + socket.bytesRead);
-  });
+  })
+  mp.pipe(file)
 });
 
 server.on('close', function(){
